1fb2f10a2eb7d65a37cecdfacd43f0b9b82f8fb5
hwaninging <hwanhee@sookmyung.ac.kr>
2025-11-16 23:42:09 +0900

    실습 7

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-//실습6
+//실습 7 
+
+char *proverb = "All that glisters is not gold.";
+void setPointer(char **q)
+{
+     *q = proverb;
+}
+
 int main(int argc, char *argv[])
 {
-     int i;
-     int *p = &i;
-     int **q = &p;
-     
-     *p = 200;
-     printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
-     
-     **q = 300;
-     printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
+    char *p = "zzz";
+    
+    setPointer(&p);
+    printf("%s \n", p);
                                
   system("PAUSE");	
   return 0;
